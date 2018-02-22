8f5cf4eea8c04f2dd640f706a00ee2544b9c180c
Alxd001 <36510643+Alxd001@users.noreply.github.com>
2018-02-21 21:26:51 -0600

    Actualizacion de Estdo

--- src/interprete/Estado.java
+++ src/interprete/Estado.java
@@ -5,12 +5,40 @@
  */
 package interprete;
 
+import java.util.HashSet;
+
 /**
  *
  * @author Fernando
  */
-public class Estado {
-    static int IdEdoAct;
+public class Estado 
+{
+    static int IdEdoAct=0;
     int IdEdo;
+    boolean EdoAcept;
+    public HashSet<Transicion> Transiciones;
+    
+    public Estado()
+    {
+        IdEdo=IdEdoAct+1;
+        EdoAcept=false;
+        Transiciones = new HashSet<Transicion>();
+        Transiciones.clear();
+    }
+    
+    public int getIdEdo() //Regresa el Id del Estado en el que nos encontramos.
+    {
+        return this.IdEdo;        
+    }
+    
+    public boolean getEdoAcept() //Regresa True o False para saber si es o no estado de aceptación.
+    {
+        return this.EdoAcept;
+    }
+    
+    public HashSet<Transicion> getTransiciones() //Regresa las transiciones que tenga este estado.
+    {
+        return this.Transiciones;
+    }
     
 }
