668eef58acfd647041883b395c948eee2e04287d
Fernando Dehesa <fernando.d.lozada@gmail.com>
2018-02-21 23:47:33 -0600

    Primeros dos botones con acciones

--- src/interprete/Interfaz.java
+++ src/interprete/Interfaz.java
@@ -5,6 +5,22 @@
  */
 package interprete;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /**
  *
  * @author FernandoEsteban
@@ -14,10 +30,81 @@ public class Interfaz extends javax.swing.JFrame {
     /**
      * Creates new form Interfaz
      */
+    private JLabel jLabel10;
+    private JLabel jLabel11;
+    private JLabel jLabel12;
+    private JLabel jLabel13;
+    private JButton jButton10;
+    private JTextField jTextField10;
+    private JPanel contenedorVentana;
+    private JPanel contenedorTop;
+    private JPanel contenedorMiddle;
+    private JPanel contenedorBottom;
+    private JComboBox select1;
+    private JComboBox select2;
+    private JComboBox select3;
+    private Ventana v;
+    
     public Interfaz() {
         initComponents();
+        inicializar();
     }
-
+    
+    private void inicializar(){
+        v = new Ventana();
+        v.setSize(310, 210);                                 // colocamos tamanio a la ventana (ancho, alto)
+        v.setLocationRelativeTo(this);                       // centramos la ventana en la pantalla
+        v.setLayout(null);                                   // no usamos ningun layout, solo asi podremos dar posiciones a los componentes
+        v.setResizable(false);                               // hacemos que la ventana no sea redimiensionable
+        v.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);    // hacemos que cuando se cierre la ventana termina todo proceso
+        this.setLocationRelativeTo(null);
+        jLabel10 = new JLabel("Insertar caracter: ");
+        jLabel11 = new JLabel("Guardar en: ");
+        jLabel12 = new JLabel(" con ");
+        jLabel13 = new JLabel("Aplicar a: ");
+        jButton10 = new JButton("OK");
+        jButton10.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton10ActionPerformed(evt);
+            }
+        });
+        jTextField10 = new JTextField();
+        contenedorVentana =  new JPanel();
+        contenedorTop =  new JPanel();
+        contenedorMiddle =  new JPanel();
+        contenedorBottom =  new JPanel();
+        contenedorBottom.setLayout(new GridLayout(1,1));
+        contenedorBottom.add(jButton10);
+        
+        select1 =  new JComboBox();
+        select1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               // Aquí hacemos lo que queramos hacer.
+            }
+        });
+        select1.addItem("Autómata 1");
+        select1.addItem("Autómata 2");
+        select1.addItem("Autómata 3");
+        select2 =  new JComboBox();
+        select2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               // Aquí hacemos lo que queramos hacer.
+            }
+        });
+        select2.addItem("Autómata 1");
+        select2.addItem("Autómata 2");
+        select2.addItem("Autómata 3");
+        select3 =  new JComboBox();
+        select3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               // Aquí hacemos lo que queramos hacer.
+            }
+        });
+        select3.addItem("Autómata 1");
+        select3.addItem("Autómata 2");
+        select3.addItem("Autómata 3");
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -50,6 +137,11 @@ public class Interfaz extends javax.swing.JFrame {
         jLabel1.setText("Operaciones");
 
         jButton1.setText("Crear AFN básico");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Unir AFN's");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -66,8 +158,18 @@ public class Interfaz extends javax.swing.JFrame {
         });
 
         jButton4.setText("Operación cerradura +");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jButton5.setText("Operación cerradura *");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
 
         jButton6.setText("Operación ?");
         jButton6.addActionListener(new java.awt.event.ActionListener() {
@@ -93,12 +195,27 @@ public class Interfaz extends javax.swing.JFrame {
 
         jButton7.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButton7.setText("Validar");
+        jButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton7ActionPerformed(evt);
+            }
+        });
 
         jButton8.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButton8.setText("Validar");
+        jButton8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton8ActionPerformed(evt);
+            }
+        });
 
         jButton9.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButton9.setText("Validar");
+        jButton9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton9ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -179,17 +296,93 @@ public class Interfaz extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        if (evt.getSource() == jButton2){
+            v.setVisible(true);
+            v.setTitle("Unir AFN's");
+            
+            contenedorTop.setLayout(new GridLayout(1,3));
+            contenedorTop.add(select1);
+            contenedorTop.add(jLabel12);
+            contenedorTop.add(select2);
+            contenedorMiddle.setLayout(new GridLayout(1,2));
+            contenedorMiddle.add(jLabel11);
+            contenedorMiddle.add(select3);
+            
+            v.setLayout(new GridLayout(3,1));
+            v.add(contenedorTop);
+            v.add(contenedorMiddle);
+            v.add(contenedorBottom);
+        }
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        if (evt.getSource() == jButton3){
+            v.setVisible(true);
+            v.setTitle("Concatenar AFN's");
+        }
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        // TODO add your handling code here:
+        if (evt.getSource() == jButton6){
+            v.setVisible(true);
+            v.setTitle("Operación ?");
+        }
     }//GEN-LAST:event_jButton6ActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        if (evt.getSource() == jButton1){            
+            v.setVisible(true);
+            v.setTitle("Crear AFN básico");                   // colocamos titulo a la ventana
+            
+            contenedorTop.setLayout(new GridLayout(1,2));
+            contenedorTop.add(jLabel10);
+            contenedorTop.add(jTextField10);
+            contenedorMiddle.setLayout(new GridLayout(1,2));
+            contenedorMiddle.add(jLabel11);
+            contenedorMiddle.add(select1);
+            //contenedorBottom.setLayout(new GridLayout(1,1));
+            //contenedorBottom.add(jButton10);
+
+            v.setLayout(new GridLayout(3,1));
+            v.add(contenedorTop);
+            v.add(contenedorMiddle);
+            v.add(contenedorBottom);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        if (evt.getSource() == jButton4){
+            v.setVisible(true);
+            v.setTitle("Operación cerradura +");
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        if (evt.getSource() == jButton5){
+            v.setVisible(true);
+            v.setTitle("Operación cerradura *");
+        }
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton7ActionPerformed
+
+    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton8ActionPerformed
+
+    private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton9ActionPerformed
+
+    private void jButton10ActionPerformed(java.awt.event.ActionEvent evt){
+        if (evt.getSource() == jButton10){
+            System.out.println("rer");
+            v.dispose();
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -225,6 +418,8 @@ public class Interfaz extends javax.swing.JFrame {
         });
     }
 
+    
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
