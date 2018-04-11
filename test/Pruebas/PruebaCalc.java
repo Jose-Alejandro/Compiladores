e76fc4e92732ab76d31cbbac8a1f63dcf93a73eb
Jose-Alejandro <Alejandro@PC-2.local>
2018-04-11 14:54:48 -0500

    calculadora con prefijo y posfijo funcionando

--- test/Pruebas/PruebaCalc.java
+++ test/Pruebas/PruebaCalc.java
@@ -9,6 +9,8 @@ public class PruebaCalc {
     Calculadora calcu=new Calculadora("LN(5)+COSENO(3.141592/4)^2+(5/9)"); //2.6649936314
     calcu.AnalizarExpr();
     System.out.println(calcu.result);
+    System.out.println(calcu.prefijo);
+    System.out.println(calcu.posfijo);
   }
 
 
