90f8b836322f6cd6525607263a65d29a430628ea
Jose-Alejandro <Alejandro@PC-2.local>
2018-04-11 14:54:48 -0500

    calculadora con prefijo y posfijo funcionando

--- src/interprete/Calculadora.java
+++ src/interprete/Calculadora.java
@@ -4,10 +4,16 @@ class Mfloat{
  public float v=0;
 }
 
+class MString{
+  public String s="";
+}
+
 
 public class Calculadora {
   Scanner Lexic;
   public float result;
+  public String posfijo="",prefijo="";
+  public MString pos=new MString(),pre=new MString();
   public Calculadora(String cadena){
     this.Lexic=new Scanner(cadena);
   }
@@ -16,10 +22,12 @@ public class Calculadora {
   {
     boolean R;
     Mfloat v=new Mfloat();
-    R=E(v);
+    R=E(v,pos,pre);
     if (R){
       if (Lexic.getToken()==Tokens.FIN){
         result=v.v;
+        prefijo=pre.s;
+        posfijo=pos.s;
         return R;
       }
       else
@@ -28,22 +36,34 @@ public class Calculadora {
     return R;
   }
 
-  boolean E(Mfloat v)
+  boolean E(Mfloat v,MString pos, MString pre)
   {
-    if (T(v))
-      if (Ep(v))
+    if (T(v,pos,pre))
+      if (Ep(v,pos,pre))
         return true;
     return false;
   }
 
-  boolean Ep(Mfloat v)
+  boolean Ep(Mfloat v,MString pos, MString pre)
   {
     Mfloat v1=new Mfloat();
+    MString pre2=new MString(),pos2=new MString();
     int tok=Lexic.getToken();
     if (tok==Tokens.MAS || tok==Tokens.MENOS){
-      if (T(v1)){
-        v.v=v.v+(tok==Tokens.MAS?v1.v:-v1.v);
-        if (Ep(v))
+      if (T(v1,pos2, pre2)){
+        switch (tok){
+          case Tokens.MAS:
+            v.v=v.v+v1.v;
+            pos.s+=" "+pos2.s+" "+"+";
+            pre.s="+ "+pre.s+" "+pre2.s;
+            break;
+          case Tokens.MENOS:
+            v.v=v.v-v1.v;
+            pos.s+=" "+pos2.s+" "+"-";
+            pre.s="- "+pre.s+" "+pre2.s;
+            break;
+        }
+        if (Ep(v,pos,pre))
           return true;
       }
       return false;
@@ -52,32 +72,39 @@ public class Calculadora {
     return true;
   }
 
-  boolean T(Mfloat v)
+  boolean T(Mfloat v,MString pos, MString pre)
   {
-    if (F(v))
-      if (Tp(v))
+    if (F(v,pos,pre))
+      if (Tp(v,pos,pre))
         return true;
     return  false;
   }
 
-  boolean Tp(Mfloat v)
+  boolean Tp(Mfloat v,MString pos,MString pre)
   {
     int tok=Lexic.getToken();
     Mfloat v1=new Mfloat();
+    MString pre2=new MString(),pos2=new MString();
     if (tok==Tokens.PROD||tok==Tokens.DIV || tok==Tokens.POTENCIA){
-      if (F(v1)){
+      if (F(v1,pos2,pre2)){
         switch (tok){
           case Tokens.PROD:
             v.v*=v1.v;
+            pos.s+=" "+pos2.s+" "+"*";
+            pre.s="* "+pre.s+" "+pre2.s;
             break;
           case Tokens.DIV:
             v.v/=v1.v;
+            pos.s+=" "+pos2.s+" "+"/";
+            pre.s="/ "+pre.s+" "+pre2.s;
             break;
           case  Tokens.POTENCIA:
             v.v= (float) Math.pow(v.v,v1.v);
+            pos.s+=" "+pos2.s+" "+"^";
+            pre.s="^ "+pre.s+" "+pre2.s;
             break;
         }
-        if (Tp(v))
+        if (Tp(v,pos,pre))
           return true;
       }
       return false;
@@ -86,7 +113,7 @@ public class Calculadora {
     return true;
   }
 
-  boolean F(Mfloat v)
+  boolean F(Mfloat v,MString pos,MString pre)
   {
     int tok=Lexic.getToken();
     switch (tok){
@@ -94,11 +121,13 @@ public class Calculadora {
         tok=Lexic.getToken();
         switch (tok){
           case Tokens.PAR_I:
-            if (E(v))
+            if (E(v,pos,pre))
             {
               tok=Lexic.getToken();
               if (tok==Tokens.PAR_D){
                 v.v= (float) Math.sin(v.v);
+                pos.s+=" "+"sin";
+                pre.s="sin "+pre.s;
                 return true;
               }
             }
@@ -108,11 +137,13 @@ public class Calculadora {
         tok=Lexic.getToken();
         switch (tok){
           case Tokens.PAR_I:
-            if (E(v))
+            if (E(v,pos,pre))
             {
               tok=Lexic.getToken();
               if (tok==Tokens.PAR_D){
                 v.v= (float) Math.cos(v.v);
+                pos.s+=" "+"cos";
+                pre.s="cos "+pre.s;
                 return true;
               }
             }
@@ -122,11 +153,13 @@ public class Calculadora {
         tok=Lexic.getToken();
         switch (tok){
           case Tokens.PAR_I:
-            if (E(v))
+            if (E(v,pos,pre))
             {
               tok=Lexic.getToken();
               if (tok==Tokens.PAR_D){
                 v.v= (float) Math.tan(v.v);
+                pos.s+=" "+"tan";
+                pre.s="tan "+pre.s;
                 return true;
               }
             }
@@ -136,11 +169,13 @@ public class Calculadora {
         tok=Lexic.getToken();
         switch (tok){
           case Tokens.PAR_I:
-            if (E(v))
+            if (E(v,pos,pre))
             {
               tok=Lexic.getToken();
               if (tok==Tokens.PAR_D){
                 v.v= (float) Math.exp(v.v);
+                pos.s+=" "+"exp";
+                pre.s="exp "+pre.s;
                 return true;
               }
             }
@@ -150,11 +185,13 @@ public class Calculadora {
         tok=Lexic.getToken();
         switch (tok){
           case Tokens.PAR_I:
-            if (E(v))
+            if (E(v,pos,pre))
             {
               tok=Lexic.getToken();
               if (tok==Tokens.PAR_D){
                 v.v= (float) Math.log(v.v);
+                pos.s+=" "+"ln";
+                pre.s="ln "+pre.s;
                 return true;
               }
             }
@@ -164,18 +201,20 @@ public class Calculadora {
         tok=Lexic.getToken();
         switch (tok){
           case Tokens.PAR_I:
-            if (E(v))
+            if (E(v,pos,pre))
             {
               tok=Lexic.getToken();
               if (tok==Tokens.PAR_D){
                 v.v= (float) Math.log10(v.v);
+                pos.s+=" "+"log";
+                pre.s="log "+pre.s;
                 return true;
               }
             }
             return false;
         }
       case Tokens.PAR_I:
-        if (E(v))
+        if (E(v,pos,pre))
         {
           tok=Lexic.getToken();
           if (tok==Tokens.PAR_D)
@@ -184,6 +223,8 @@ public class Calculadora {
         return false;
       case Tokens.NUM:
         v.v=Float.parseFloat(Lexic.getLexema());
+        pos.s+=Lexic.getLexema();
+        pre.s+=Lexic.getLexema();
         return true;
     }
     return false;
--- test/Pruebas/PruebaCalc.java
+++ test/Pruebas/PruebaCalc.java
@@ -6,9 +6,11 @@ import interprete.Scanner;
 public class PruebaCalc {
 
   public static void main(String[] args) {
-    Calculadora calcu=new Calculadora("LN(5)+COSENO(3.141592/4)^2+(5/9)"); //2.6649936314
+    Calculadora calcu=new Calculadora("LN(5)+COSENO(3.141592/4)^2-(5/9)"); //2.6649936314
     calcu.AnalizarExpr();
     System.out.println(calcu.result);
+    System.out.println(calcu.prefijo);
+    System.out.println(calcu.posfijo);
   }
 
 
