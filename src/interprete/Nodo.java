5ea0afa315de172dd4be2d9d6ef0dae6e155cc86
FStefanLE <stefan170992@gmail.com>
2018-05-01 13:13:43 -0500

    First y Follow

--- src/interprete/Nodo.java
+++ src/interprete/Nodo.java
@@ -1,11 +1,29 @@
 package interprete;
 
+import java.util.ArrayList;
+
 public class Nodo {
     
     String simbolo;
     Nodo ApAbajo = null;
     Nodo ApSiguiente = null;
     boolean fin;
+
+    public Nodo getApAbajo() {
+        return ApAbajo;
+    }
+
+    public void setApAbajo(Nodo ApAbajo) {
+        this.ApAbajo = ApAbajo;
+    }
+
+    public Nodo getApSiguiente() {
+        return ApSiguiente;
+    }
+
+    public void setApSiguiente(Nodo ApSiguiente) {
+        this.ApSiguiente = ApSiguiente;
+    }
     
     public Nodo(String simbolo, boolean fin) {
         this.simbolo = simbolo;
@@ -17,6 +35,17 @@ public class Nodo {
         this.ApAbajo = abajo;
         this.ApSiguiente = siguiente;
         this.fin = fin;
+    }
+    
+    public ArrayList<Nodo> getNodosAbajo() {
+        ArrayList nodos = new ArrayList();
+        Nodo n = this;
+        nodos.add(n);
+        while(n.ApAbajo != null) {
+            nodos.add(n.ApAbajo);
+            n = n.ApAbajo;
+        }
+        return nodos;
     }
     
 }
