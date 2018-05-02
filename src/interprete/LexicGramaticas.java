b47c72e7186ee62b124b84ca1d6b07001904a9a3
Alxd001 <36510643+Alxd001@users.noreply.github.com>
2018-05-02 11:55:18 -0500

    Agregacion Lexic Gramaticas

---
+++ src/interprete/LexicGramaticas.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interprete;
+import java.util.Stack;
+
+/**
+ *
+ * @author Alxd001
+ */
+
+public class LexicGramaticas 
+{
+    public String Lexema;
+    public int Token;
+    Stack<String> pilaToken;
+    
+    public LexicGramaticas (String cadena)
+    {
+        this.Lexema=cadena;
+        this.Token=1000; //Error
+        pilaToken = new Stack<>();
+    }
+    
+    public void EscanerLexic()
+    {
+        int i;
+        char cadenaLexema[];
+        int longitudLexema=Lexema.length();
+        cadenaLexema=Lexema.toCharArray();
+        
+        for(i=0; i<longitudLexema; i++)
+        {
+            while (i < longitudLexema &&(cadenaLexema[i] == ' ' || cadenaLexema[i] == '\n')) 
+            {
+                i++;
+            }
+            if (EsLetra(cadenaLexema[i])) 
+            {
+                Lexema = "";
+                while (EsLetra(cadenaLexema[i]) || EsDigito(cadenaLexema[i]))   
+                {
+                    i++;
+                }
+                Token = TokensGramaticas.SIMB;
+                pilaToken.push(Integer.toString(Token)); //Agregamos a la pila el Token.
+            }
+            if(PC(cadenaLexema[i]))
+            {
+                Token=TokensGramaticas.PC;
+                pilaToken.push(Integer.toString(Token)); //Agregamos a la pila el Token.
+            }
+            else if(OR(cadenaLexema[i]))
+            {
+                Token=TokensGramaticas.OR;
+                pilaToken.push(Integer.toString(Token)); //Agregamos a la pila el Token.
+            }
+            else if((cadenaLexema[i] == '-') && cadenaLexema[i+1] == '>')
+            {
+                Token=TokensGramaticas.FLECHA;
+                pilaToken.push(Integer.toString(Token)); //Agregamos a la pila el Token.
+                i+=1;
+            }
+            else if(cadenaLexema[i] == '€')
+            {
+                Token=TokensGramaticas.EPSILON;
+                pilaToken.push(Integer.toString(Token)); //Agregamos a la pila el Token.
+            }
+        }
+    }
+    
+    public boolean PC(char c)
+    {
+        return c == ';'; //Regresa TRUE si el caracter es ;
+    }
+    
+    public boolean OR(char c)
+    {
+        return c == '|'; //Regresa True si el caracter es |
+    }
+    
+    public boolean Simb(char c)
+    {
+        return (c>='A' && c<='Z');
+    }
+    
+    boolean EsDigito(char c)
+    {
+        return (c>='0' && c<='9' );
+    }
+
+    boolean EsLetra(char c)
+    {
+        return (c>='a' && c<='z' ) || (c>='A' && c<='Z' );
+    }
+    
+    public Stack<String> getEdo()
+    {
+        return pilaToken;
+    }
+    
+    public void setEdo(Stack<String> nuevaPila)
+    {
+        pilaToken=nuevaPila;
+    }
+    
+    public int getToken()
+    {
+        int TokenReturn;
+        TokenReturn = Integer.parseInt(pilaToken.pop());
+        return TokenReturn;
+    }
+    
+    public void returnToken(int TokenReturn)
+    {
+        pilaToken.push(String.valueOf(TokenReturn));
+    }
+    
+    public void ImprimeTokens() 
+    {
+        Stack<String> pilaTokencopia;
+        pilaTokencopia = pilaToken;
+        while (!pilaTokencopia.empty()) 
+        {
+            System.out.println(pilaTokencopia.pop() + ",");
+        }
+    }
+}
---
+++ src/interprete/TokensGramaticas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interprete;
+
+/**
+ *
+ * @author Alxd001
+ */
+public class TokensGramaticas 
+{
+    public static int FIN = 0;
+    public static int ERROR = -1;
+    public static int SIMB = 10;
+    public static int PC = 20;
+    public static int OR = 30;
+    public static int FLECHA = 40;
+    public static int SimboloPrimo = 50;
+    public static int EPSILON = 100;
+    public static String Espacio = null;
+        
+}
